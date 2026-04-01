166d4a289f533e4e1c37174cf7922d109325b764
BrunoOka <brunohideki31@gmail.com>
2026-04-01 10:29:18 -0300

    Rename calculo.py to ex2.py

--- calculo.py
+++
@@ -1,3 +0,0 @@
-num1 = int(input("Digite um número inteiro: "))
-num2 = int(input("Digite outro número inteiro: "))
-print(f'A soma dos números é {num1 + num2}, a subtração dos números é {num1 - num2}, a multiplicação dos número é {num1 * num2}, a divisão dos números é {num1 / num2:.1f}')
