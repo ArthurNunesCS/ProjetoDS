e91a4b77bc36dfdddd76cdf73218e13b20881ad6
Arthur <nunesarthur768@gmail.com>
2026-04-10 10:21:58 -0300

    fazendo exes 14, 15, 16

---
+++ 16.py
@@ -0,0 +1,8 @@
+v = float(input('digite o valor do produto: '))
+qtd = int(input('digite a quantidade do produto: '))
+
+def pag(v,qtd):
+    paga = v * qtd
+    print(paga)
+    
+pag(v, qtd)
---
+++ ex15.py
@@ -0,0 +1,8 @@
+i = int(input("digite sua idade: "))
+def midade(i):
+    if i>=18:
+        print('maior de idade')
+    else:
+        print('menor de idade')
+
+midade(i)
